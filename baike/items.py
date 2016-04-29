e8d96bee7aa443eb667ff5478115deba099e40af
Xiang Zhang <zxteloiv@outlook.com>
2016-04-29 14:35:59 +0800

    fix the python import dir problem by renaming the crawler file into another name

--- baike/items.py
+++ baike/items.py
@@ -7,10 +7,8 @@
 
 import scrapy
 
-
 class BaikeItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
     itemid = scrapy.Field()
     itemtype = scrapy.Field()
     url = scrapy.Field()
--- baike/spiders/htmlpage.py
+++ baike/spiders/htmlpage.py
@@ -1,24 +1,27 @@
 import scrapy
+from baike.items import BaikeItem
 
 class BaikeSpider(scrapy.Spider):
     name = "baike"
     allowed_domains = ["baike.baidu.com"]
     start_urls = (
-        "http://baike.baidu.com/dili",
-        "http://baike.baidu.com/shenghuo",
-        "http://baike.baidu.com/wenhua",
-        "http://baike.baidu.com/ziran",
-        "http://baike.baidu.com/shehui"
+        "http://baike.baidu.com/view/486340.htm",
+        #"http://baike.baidu.com/dili",
+        #"http://baike.baidu.com/shenghuo",
+        #"http://baike.baidu.com/wenhua",
+        #"http://baike.baidu.com/ziran",
+        #"http://baike.baidu.com/shehui"
     )
 
     def parse(self, response):
         # save data
         if not self.is_error(response):
-            item = parse_page(self, response)
+            item = self.parse_page(response)
             if item is not None:
                 yield item
 
         # follow links
+        """
         for link in response.xpath("//a[@href]"):
             anchor = link.xpath("text()").extract()[0].strip()
             href = link.xpath("@href").extract()[0]
@@ -29,6 +32,7 @@ class BaikeSpider(scrapy.Spider):
             url = response.urljoin(href)
             meta = {"anchor_text":anchor}
             yield scrapy.Request(url, callback=self.parse, meta=meta)
+        """
 
     def is_error(self, response):
         return len(response.body) < 25000 or response.status != 200
